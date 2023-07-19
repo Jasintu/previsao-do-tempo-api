6a8a28fd03bdeeea54d1a2310ba58038852c09ac
Jasintu <128087894+Jasintu@users.noreply.github.com>
2023-07-18 23:52:25 -0300

    UP

--- script/index.js
+++ script/index.js
@@ -5,16 +5,27 @@ let key = "5bfba1b82e310a266ce5d6a91be86f71"
 /*------------------------------------------------------------*/
 $("#buttonSubmit").click(function (e) { 
   e.preventDefault()
+  //requisiçao ajax ----------------------------------------------
   $.ajax({
     type: "get",
     url: `https://api.openweathermap.org/data/2.5/weather?q=${$("#input-info").val()}&units=metric&appid=${key}&lang=pt_br`,
     data: "data",
+    //---------------------------------------------------------------------
     success: function (response) {
       console.log(response)
-      console.log(response.name)
-      console.log(response.main.temp)
+      //exibiçao de temperatura
       $("#showTemp").text(Math.round(response.main.temp) + "°C")
+      //exibiçao nome do pais
+      $("#country").text(response.name + ",")
+      //exibiçao sigla
+      $("#sigle").text(response.sys.country + " 📍")
+      //exibiçao 
+      $("#weather").text(response.weather[0].main)
+
+      //-----------------------------------------------------------------------------
     }
   })
 
 })
+
+
